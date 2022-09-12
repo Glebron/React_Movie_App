c2de332890149ce0caacc5de7e30a276cc21db0d
Gleb Tsurakov <34337704+Glebron@users.noreply.github.com>
2022-09-12 20:16:44 +0300

    push_1.1

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import MovieModal from "./MovieModal";
 
 function MovieList({ filtered }) {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState([]);
+
   return (
     <div className="content-wrapper">
+      {modalOpen ? (
+        <div>
+          <div className="bg"></div>
+          <MovieModal
+            setModalOpen={setModalOpen}
+            modalOpen={modalOpen}
+            modalContent={modalContent}
+          ></MovieModal>
+        </div>
+      ) : (
+        <div></div>
+      )}
       <motion.div
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
@@ -13,7 +30,16 @@ function MovieList({ filtered }) {
         <AnimatePresence>
           <div className="movie-grid">
             {filtered.map((movie) => {
-              return <MovieCard key={movie.id} movie={movie}></MovieCard>;
+              return (
+                <MovieCard
+                  modalContent={modalContent}
+                  setModalContent={setModalContent}
+                  modalOpen={modalOpen}
+                  setModalOpen={setModalOpen}
+                  key={movie.id}
+                  movie={movie}
+                ></MovieCard>
+              );
             })}
           </div>
         </AnimatePresence>
